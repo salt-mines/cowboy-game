b7f449ed9443dc797ef824811a7970f3713b865f
Aleksi Kervinen <me@aleksi.me>
2019-06-12 14:56:26 +0300

    improve camera tracking and reorganize assets

--- Assets/Scripts/CameraTracker.cs
+++ Assets/Scripts/CameraTracker.cs
@@ -1,19 +1,45 @@
 ﻿using UnityEngine;
 
+[RequireComponent(typeof(Camera))]
 public class CameraTracker : MonoBehaviour
 {
     public Transform whatToTrack;
-    public float trackingTime = 0.2f;
 
-    private Vector3 currentSmoothVel;
+    public float leftEdgeDistance = 0.3f;
+    public float rightEdgeDistance = 0.3f;
+
+    private new Camera camera;
+
+    private void Start()
+    {
+        camera = GetComponent<Camera>();
+    }
 
     void LateUpdate()
     {
-        float origZ = transform.position.z;
+        Vector3 newPos = transform.position;
 
-        var newPos = Vector3.SmoothDamp(transform.position, whatToTrack.position, ref currentSmoothVel, trackingTime);
-        newPos.z = origZ;
+        var posOnCamera = camera.WorldToViewportPoint(whatToTrack.position);
 
+        float leftLimit = leftEdgeDistance;
+        float rightLimit = 1.0f - rightEdgeDistance;
+        float diff;
+
+        if (posOnCamera.x < leftLimit)
+        {
+            diff = leftLimit - posOnCamera.x;
+        }
+        else if (posOnCamera.x > rightLimit)
+        {
+            diff = rightLimit - posOnCamera.x;
+        }
+        else
+        {
+            diff = 0;
+        }
+
+        posOnCamera.x = 0.5f - diff;
+        newPos.x = camera.ViewportToWorldPoint(posOnCamera).x;
         transform.position = newPos;
     }
 }
