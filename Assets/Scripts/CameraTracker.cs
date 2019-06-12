37b95fded767d20dcd078b9cf4f33ac69397da76
Aleksi Kervinen <me@aleksi.me>
2019-06-12 12:44:17 +0300

    add smooth camera tracking

---
+++ Assets/Scripts/CameraTracker.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+public class CameraTracker : MonoBehaviour
+{
+    public Transform whatToTrack;
+    public float trackingTime = 0.2f;
+
+    private Vector3 currentSmoothVel;
+
+    void LateUpdate()
+    {
+        float origZ = transform.position.z;
+
+        var newPos = Vector3.SmoothDamp(transform.position, whatToTrack.position, ref currentSmoothVel, trackingTime);
+        newPos.z = origZ;
+
+        transform.position = newPos;
+    }
+}
