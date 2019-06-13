eda98555a444fef033e31ffef2006c2bb786516d
Aleksi Kervinen <me@aleksi.me>
2019-06-13 12:09:58 +0300

    add lvl 1, mark prefabs static, add camera bounds

--- Assets/Scripts/CameraTracker.cs
+++ Assets/Scripts/CameraTracker.cs
@@ -8,6 +8,9 @@ public class CameraTracker : MonoBehaviour
     public float leftEdgeDistance = 0.3f;
     public float rightEdgeDistance = 0.3f;
 
+    public Transform leftBound;
+    public Transform rightBound;
+
     private new Camera camera;
 
     private void Start()
@@ -41,5 +44,29 @@ public class CameraTracker : MonoBehaviour
         posOnCamera.x = 0.5f - diff;
         newPos.x = camera.ViewportToWorldPoint(posOnCamera).x;
         transform.position = newPos;
+
+        if (leftBound)
+        {
+            var leftBoundViewportPos = camera.WorldToViewportPoint(leftBound.position);
+            if (leftBoundViewportPos.x > 0)
+            {
+                // left bound is on screen
+                posOnCamera.x = 0.5f + leftBoundViewportPos.x;
+                newPos.x = camera.ViewportToWorldPoint(posOnCamera).x;
+                transform.position = newPos;
+            }
+        }
+
+        if (rightBound)
+        {
+            var rightBoundViewportPos = camera.WorldToViewportPoint(rightBound.position);
+            if (rightBoundViewportPos.x < 1)
+            {
+                // left bound is on screen
+                posOnCamera.x = rightBoundViewportPos.x - 0.5f;
+                newPos.x = camera.ViewportToWorldPoint(posOnCamera).x;
+                transform.position = newPos;
+            }
+        }
     }
 }
