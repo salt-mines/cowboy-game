fd094dbdd5db832d40619a198eb8fcec27b22157
Aleksi Kervinen <me@aleksi.me>
2019-06-13 13:43:28 +0300

    add pause and game over menus

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,6 +1,6 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
 {
@@ -8,15 +8,63 @@ public class GameManager : MonoBehaviour
 
     public GameObject playerSpawnpoint;
 
+    // Menus
+    public Canvas canvas;
+    public EventSystem eventSystem;
+
+    public GameObject pauseMenuPrefab;
+    public GameObject gameOverMenuPrefab;
+
+    private bool isGameOver = false;
+    private bool paused = false;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        Time.timeScale = 1;
     }
 
-    // Update is called once per frame
-    void Update()
+    void Awake()
     {
-        
+        var gm = GameObject.FindGameObjectsWithTag("GameController");
+
+        if (gm.Length > 1)
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    public void OpenPauseMenu()
+    {
+        if (paused) return;
+
+        Pause();
+
+        GameObject menu = Instantiate(pauseMenuPrefab, canvas.gameObject.transform);
+        menu.GetComponent<PauseMenu>().gameManager = this;
+        eventSystem.SetSelectedGameObject(menu.transform.GetChild(0).gameObject);
+    }
+
+    public void Pause()
+    {
+        if (paused) return;
+
+        paused = true;
+
+        Time.timeScale = 0;
+    }
+
+    public void Resume()
+    {
+        if (!paused) return;
+
+        paused = false;
+
+        Time.timeScale = 1;
+    }
+
+    public void Restart()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
     }
 }
